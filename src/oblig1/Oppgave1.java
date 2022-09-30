93e1034d46b8247a6601530d7a83185d8e11a578
s375120 <s375120@oslomet.no>
2022-09-30 13:00:02 +0200

    Initial commit

---
+++ src/oblig1/Oppgave1.java
@@ -0,0 +1,35 @@
+package oblig1;
+
+public class Sirkel {
+}
+
+// oppgaveteks: Nedenfor ser du en skisse for klassen Sirkel. Oppgaven er å erstatte skallkoden med java-kode.
+    // Statisk metode som beregner og returnerer sirkelens diameter.
+
+    // Statisk metode som beregner og returnerer sirkelens omkrets.
+    // Omkrets = 2*pi*r
+
+    /* Statisk metode som beregner og returnerer sirkelens areal.
+       Areal = pi*r^2
+       Math.PI kan brukes for en konstant for PI
+       Math.pow kan brukes for å opphøye et tall i en potens
+     */
+}
+
+// oppgavetekst: Nedenfor ser du en skisse av hovedprogrammet som du skal bruke til å teste ut
+// klassen Sirkel. Oppgaven er også her å erstatte skallkoden med java-kode.
+public class Oblig1Oppgave1
+{
+    public static void main(String[] args)  {
+        //  Sett en variabel lik en verdi for radius
+
+    /* Kall så de statiske metodene i sirkel-klassen for å vise følgende på
+       System.out (gitt radius lik 23.5)
+
+       Arealet av sirkelen med radius 23.5 er 1734,94
+       Omkretsen av sirkelen med radius 23.5 er 147,65
+       Diameteren til sirkelen er med radius 23.5 er 47,00
+
+       Tips : String.format("%.2f",desimalverdi); for å formatere to desimaler
+    */
+    }
---
+++ src/oblig1/Oppgave2.java
@@ -0,0 +1,40 @@
+package oblig1;
+
+public class Oppgave2 {
+}
+   // OPPGAVETEKST: Det skal lages en klasse kalt Vare. Oppgaven er å erstatte skallkoden med java-kode.
+public class Vare {
+    private String navn;
+    private int antall;
+    private double pris;
+
+    // Konstruktør med alle attributter
+
+    // get / set metoder
+    // antall og pris må være større enn null
+
+    // metode for å beregne totalpris (pris * antall)
+}
+
+// OPPGAVETEKST: I main-metoden skal det leses inn 2 varer via showInputDialog.
+// Det skal sjekkes om antall og pris blir konvertert riktig med try/catch.
+// Nedenfor er den en skallkode for hovedprogrammet som du skal bruke til å teste ut klassen Vare.
+// Oppgaven er også her å erstatte skissen med java-kode.
+//Det skal altså opprettes to objekter med de innleste verdiene og så skal det beregnes en totalpris
+// for alle varene ved hjelp av metoden i klassen Vare.. Totalprisen skal vises slik (på System.out):
+//Totalprisen ble : 125.0 kr
+public class Oblig1Oppgave2 {
+
+    public static void main(String[] args){
+        // les inn navn, antall og pris for vare 1
+
+        // les inn navn, antall og pris for vare 2
+
+        // bruk avvikshåntering (try/catch) for å konvertere
+        // antall og pris for vare 1 og vare 2
+
+        // opprett to vare-objekter med de innleste verdiene
+
+        // beregn og skriv ut total pris for alle varer i System.out
+    }
+}
